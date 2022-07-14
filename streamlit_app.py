2690a851ce7abc370d4b0af27563093d661f894a
Sha <109242829+shalinidelabs@users.noreply.github.com>
2022-07-13 20:35:41 -0400

    Update streamlit_app.py
    Update menu to favorites, added icon and a new item

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu', anchor=None)
-streamlit.text('Omega 3 & Bluebery Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-
+streamlit.header('Breakfast Favorites', anchor=None)
+streamlit.text('🥣 Omega 3 & Bluebery Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text(' 🥑🍞 Avocado Toast')
